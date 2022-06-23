ea3e2e016416117ac9fb2bd94afb3a6b116b0f37
Amir Pirnazarov <pirnazarova988@gmail.com>
2022-06-23 10:01:49 +0500

    accept file name

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ class File_action:
 
     def open_file(self):
         file = open(f"{self.name_file}.txt", 'r')
+        print("------")
         print("\n", file.read())
+        print("------")
+        file.close()
 
 
-name_f = input("Введите имя файла \n")
-
-file = File_action(name_f)
-file.open_file()
-
+while True:
+    name_f = input("Введите имя файла \n")
+    try:
+        file = File_action(name_f)
+        file.open_file()
+        break
+    except FileNotFoundError:
+        print(f"Файл под {name_f} - именнем не существует")
 
