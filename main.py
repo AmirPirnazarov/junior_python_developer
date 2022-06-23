6a6b8d29ddb46964511bba867ae9fc4b90079e74
Amir Pirnazarov <pirnazarova988@gmail.com>
2022-06-23 10:57:49 +0500

    function Change on list

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ class File_action:
         print("Добавленно!")
         file.close()
 
+    def change_fiel(self, number_line, change_text):
+        file = open('a.txt', 'r')
+        lines = file.readlines()
+        file.close()
+        file = open('a.txt', 'r')
+        filedata = file.read()
+        file.close()
+        number_line = number_line - 1
+        text_1 = lines[number_line]
+        filedata = filedata.replace(text_1, f"{change_text}\n")
+        file = open('a.txt', 'w')
+        file.write(filedata)
+
 
 def action_on_file():
     while True:
@@ -24,6 +37,13 @@ def action_on_file():
         if int(action) == 1:
             add_arr = input("Введите то что ходите добавить в список.\n")
             file.add_file(add_arr)
+
+        elif int(action) == 2:
+            number_line = input("Введите какую строку хотитие изменить.\n")
+            text_change = input("Введите текст\n")
+            file.change_fiel(int(number_line), text_change)
+            print("Изменнно!")
+
         else:
             print("Я вас не понимаю")
 
