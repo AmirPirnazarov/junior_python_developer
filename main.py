2fd49b7bea22fc0a597c97c6435da68b810f771c
Amir Pirnazarov <pirnazarova988@gmail.com>
2022-06-23 11:16:21 +0500

    program check output exception

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-class File_action:
+class File_action():
     def __init__(self, name_file):
         self.name_file = name_file
 
     def open_file(self):
         file = open(f"{self.name_file}.txt", 'r')
-        print("------")
+        print("--- Начало списка ---")
         print("\n", file.read())
-        print("------")
+        print("--- Конец списка ---\n")
         file.close()
 
     def add_file(self, text):
@@ -14,19 +14,6 @@ class File_action:
         file.write("\n" + text)
         print("Добавленно!")
         file.close()
-
-    def change_fiel(self, number_line, change_text):
-        file = open('a.txt', 'r')
-        lines = file.readlines()
-        file.close()
-        file = open('a.txt', 'r')
-        filedata = file.read()
-        file.close()
-        number_line = number_line - 1
-        text_1 = lines[number_line]
-        filedata = filedata.replace(text_1, f"{change_text}\n")
-        file = open('a.txt', 'w')
-        file.write(filedata)
 
     def delate_file(self, namber_line):
         file = open('a.txt', 'r')
@@ -78,19 +65,41 @@ def action_on_file():
             file.add_file(add_arr)
 
         elif int(action) == 2:
-            number_line = input("Введите какую строку хотитие изменить.\n")
-            text_change = input("Введите текст\n")
-            file.change_fiel(int(number_line), text_change)
-            print("Изменнно!")
+            while True:
+                number_line = input("Введите какую строку хотитие изменить.\n")
+                if number_line.isdigit():
+                    try:
+                        text_change = input("Введите текст\n")
+                        file.change_fiel(int(number_line), text_change)
+                        print("Изменнно!\n")
+                        break
+                    except IndexError:
+                        print("Вы вводите номер строки которого нет в файле!\n")
+                else:
+                    print("Вы ввели что то не то\n")
 
         elif int(action) == 3:
-            number_line = input("Введите какую строку хотитие удалить.\n")
-            file.delate_file(int(number_line))
-            print("Удаленно!")
+            while True:
+                number_line = input("Выберете какую строку хотитие удалить.\n")
+                if number_line.isdigit():
+                    try:
+                        file.delate_file(int(number_line))
+                        print("Удаленно!\n")
+                        break
+                    except IndexError:
+                        print("Вы вводите номер строки которого нет в файле!\n")
+                else:
+                    print("Вы ввели что то не то\n")
+
 
         elif int(action) == 4:
             file.total_amount()
 
+        elif int(action) == 5:
+            file.open_file()
+
+        elif int(action) == 0:
+            exit(0)
         else:
             print("Я вас не понимаю")
 
@@ -104,4 +113,3 @@ while True:
         break
     except FileNotFoundError:
         print(f"Файл под {name_f} - именнем не существует")
-
