992e7ed8e3b15af7a14dd02d5ca1e1b382b975d4
Amir Pirnazarov <pirnazarova988@gmail.com>
2022-06-23 10:45:27 +0500

    function Add to list

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ class File_action:
         print("------")
         file.close()
 
+    def add_file(self, text):
+        file = open(f"{self.name_file}.txt", 'a')
+        file.write("\n" + text)
+        print("Добавленно!")
+        file.close()
+
+
+def action_on_file():
+    while True:
+        action = input("какое действие совершить с файлом: \n 1 - Добавить в список \n 2 - Изменить запись в списке"
+                       "\n 3 - Удалить из списка \n 4 - Вычесть общую сумму\n 5 - Посмотреть список\n 0 - выход \n")
+
+        if int(action) == 1:
+            add_arr = input("Введите то что ходите добавить в список.\n")
+            file.add_file(add_arr)
+        else:
+            print("Я вас не понимаю")
+
 
 while True:
     name_f = input("Введите имя файла \n")
     try:
         file = File_action(name_f)
         file.open_file()
+        action_on_file()
         break
     except FileNotFoundError:
         print(f"Файл под {name_f} - именнем не существует")
